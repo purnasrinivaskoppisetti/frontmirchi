1018a255506ecaf107410a3315698971d5166af7
purnasrinivaskoppisetti <163103197+purnasrinivaskoppisetti@users.noreply.github.com>
2026-05-07 23:33:51 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,18 @@ export default function Header() {
   };
 
   return (
-    <div className=" bg-red-700 text-white py-3 px-4 ">
+    <div className="
+  fixed
+  top-0
+  left-0
+  right-0
+  z-50
+  bg-red-700
+  text-white
+  py-3
+  px-4
+  shadow-md
+">
 
      <div className='w-[80%] m-auto flex justify-between items-center'>
        {/* TITLE */}
